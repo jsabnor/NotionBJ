9834edb12bc75471a874fb718524eec14a06adce
Josema <josema.saborido@gmail.com>
2021-11-04 15:25:12 +0100

    Initial commit

---
+++ src/main/java/com/equipo2/NotionBJ/NotionBjApplication.java
@@ -0,0 +1,13 @@
+package com.equipo2.NotionBJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotionBjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotionBjApplication.class, args);
+	}
+
+}
